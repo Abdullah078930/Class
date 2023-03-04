50e75950845bca86a0615a91233ffff1b136e31d
sahil <abdullahsahil2025@gmail.com>
2023-03-04 07:55:05 -0800

    update

--- Git2.java
+++ Git2.java
@@ -5,6 +5,15 @@ public class Git2 {
 
     
     public static void main(String[] args) {
+        
+        System.out.println("Hello sahil");
+        
+        int num1 =10, num2=30;
+        int num3;
+        num3= num1+num2;
+        
+        System.out.println("The sumation is "+ num3);
+        
       
     }
     
